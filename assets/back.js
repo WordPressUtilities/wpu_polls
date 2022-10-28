55002ea7bb6730c5e9f9fae10ee9c459b548cb7a
Kevin <kevin@rocher-dev.com>
2022-10-28 10:36:02 +0200

    v 0.0.3
    - Add basic quiz editor.

--- assets/back.js
+++ assets/back.js
@@ -1,10 +1,51 @@
 document.addEventListener('DOMContentLoaded', function() {
+    'use strict';
     /* Hide metabox layout */
-    jQuery('#wpu-polls-box-id').each(function(){
+    jQuery('#wpu-polls-box-id').each(function() {
         jQuery(this).removeClass('postbox');
         jQuery(this).find('.postbox-header').remove();
     });
 
-    jQuery('.wpu-polls-answers').sortable();
+    var $answers = jQuery('.wpu-polls-answers'),
+        $button_add = jQuery('#wpu-polls-answer-add-line');
 
+    /* Allow sortable answers */
+    $answers.sortable();
+
+    /* Add a line function */
+    function add_line() {
+        var _uniqid = 't' + Math.random().toString(12).slice(2, 12) + Date.now();
+        var _tpl_content_line = jQuery('#wpu-polls-answer-template').html().replace('##uniqid##', _uniqid);
+        /* Remove template vars */
+        _tpl_content_line = _tpl_content_line.replace(/##[a-z]+##/g, '');
+        /* Insert line */
+        var $new_line = jQuery(_tpl_content_line);
+        $answers.append($new_line);
+        $new_line.find('input.answer-text').focus();
+    }
+
+    /* Add a line : button click */
+    $button_add.on('click', function(e) {
+        e.preventDefault();
+        add_line();
+    });
+
+    /* Add a line : press enter */
+    $answers.on('keydown', function(e) {
+        if (e.keyCode == 13) {
+            e.preventDefault();
+            add_line();
+        }
+    });
+
+    /* Remove a line */
+    $answers.on('click', '.delete-line', function(e) {
+        e.preventDefault();
+        jQuery(this).closest('.answer-line').remove();
+    });
+
+    /* Default Answer */
+    if (!$answers.children().length) {
+        add_line();
+    }
 });
--- wpu_polls.php
+++ wpu_polls.php
@@ -4,7 +4,7 @@ Plugin Name: WPU Polls
 Plugin URI: https://github.com/WordPressUtilities/wpu_polls
 Update URI: https://github.com/WordPressUtilities/wpu_polls
 Description: WPU Polls handle simple polls
-Version: 0.0.2
+Version: 0.0.3
 Author: darklg
 Author URI: https://darklg.me/
 License: MIT License
@@ -12,7 +12,7 @@ License URI: https://opensource.org/licenses/MIT
 */
 
 class WPUPolls {
-    private $plugin_version = '0.0.2';
+    private $plugin_version = '0.0.3';
     private $plugin_settings = array(
         'id' => 'wpu_polls',
         'name' => 'WPU Polls'
@@ -113,41 +113,125 @@ class WPUPolls {
         ));
     }
 
-    /* ADMIN */
+    /* ----------------------------------------------------------
+      Edit & Save post
+    ---------------------------------------------------------- */
+
+    /* Edit poll
+    -------------------------- */
+
     function edit_page_poll($post) {
-        echo '<table class="widefat">';
+        $question = get_post_meta($post->ID, 'wpu_polls__question', 1);
+        $answers = $this->get_post_answers($post->ID);
+
+        /* Question */
+        echo '<p><label for="wpu-polls-question">' . __('Question:', 'wpu_polls') . '</label><br /><input type="text" name="wpu_polls_question" id="wpu-polls-question" value="' . esc_attr($question) . '" /></p>';
+
+        /* Answers */
+        echo '<table class="widefat striped">';
         echo '<thead>';
         echo '<tr>';
         echo '<th colspan="2">' . __('Answer', 'wpu_polls') . '</th>';
-        echo '<th>' . __('Image', 'wpu_polls') . '</th>';
+        echo '<th colspan="2">' . __('Image', 'wpu_polls') . '</th>';
         echo '</tr>';
         echo '</thead>';
-        echo '<tbody class="wpu-polls-answers">';
-        for ($i = 0; $i < 2; $i++) {
-            echo '<tr>';
-            echo '<td><span class="dashicons dashicons-menu-alt2"></span></td>';
-            echo '<td><input name="wpu_polls_uniqid[]" type="text" /><input name="wpu_polls_answer[]" type="text" /></td>';
-            echo '<td><input name="wpu_polls_answer_image[]" type="number" /></td>';
-            echo '</tr>';
+        echo '<tbody class="wpu-polls-answers" id="wpu-polls-answers">';
+        foreach ($answers as $answer) {
+            echo $this->get_template_answer($answer);
         }
         echo '</tbody></table>';
+
+        /* Add a line */
+        echo '<p><button type="button" id="wpu-polls-answer-add-line">' . __('Add a line', 'wpu_polls') . '</button></p>';
+
+        /* Hidden fields */
         wp_nonce_field('wpu_polls_post_form', 'wpu_polls_post_form_nonce');
+        echo '<script type="text/template" id="wpu-polls-answer-template">' . $this->get_template_answer() . '</script>';
     }
 
-    /* Save poll */
+    /* Answer template */
+
+    function get_template_answer($vars = array()) {
+        $template = '';
+        $template .= '<tr class="answer-line">';
+        $template .= '<td><span class="dashicons dashicons-menu-alt2"></span></td>';
+        $template .= '<td><input name="wpu_polls_uniqid[]" type="hidden" value="##uniqid##" /><input class="answer-text" name="wpu_polls_answer[]" type="text" value="##answer##" /></td>';
+        $template .= '<td><input name="wpu_polls_answer_image[]" type="number" value="##image##" /></td>';
+        $template .= '<td><button class="delete-line" title="' . esc_attr(__('Delete this line', 'wpu_polls')) . '">&times;</button></td>';
+        $template .= '</tr>';
+        foreach ($vars as $key => $var) {
+            $template = str_replace('##' . $key . '##', $var, $template);
+        }
+
+        return $template;
+    }
+
+    /* Getter */
+    function get_post_answers($post_id) {
+        $answers = get_post_meta($post_id, 'wpu_polls__answers', 1);
+        if (!is_array($answers)) {
+            $answers = array();
+        }
+        return $answers;
+    }
+
+    /* Save poll
+    -------------------------- */
+
     function save_poll($post_id) {
+        $post_keys = array(
+            'wpu_polls_uniqid',
+            'wpu_polls_answer',
+            'wpu_polls_answer_image'
+        );
+
+        /* Empty or invalid post response */
         if (empty($_POST)) {
             return;
         }
-        if (wp_is_post_revision($post_id)) {
-            return;
+        foreach ($post_keys as $post_key) {
+            if (!isset($_POST[$post_key]) || !is_array($_POST[$post_key])) {
+                return;
+            }
         }
+
+        /* Invalid rights */
         if (!isset($_POST['wpu_polls_post_form_nonce']) || !wp_verify_nonce($_POST['wpu_polls_post_form_nonce'], 'wpu_polls_post_form')) {
             wp_nonce_ays('');
         }
-        echo '<pre>';
-        var_dump($_POST);
-        echo '</pre>';die;
+
+        /* Save answers */
+        $answers = array();
+        foreach ($_POST['wpu_polls_answer'] as $i => $answer) {
+            $answer = esc_html($answer);
+            if (!isset($_POST['wpu_polls_uniqid'][$i])) {
+                return;
+            }
+            if (!isset($_POST['wpu_polls_answer_image'][$i])) {
+                return;
+            }
+            $uniqid = $_POST['wpu_polls_uniqid'][$i];
+            if (!$uniqid) {
+                $uniqid = md5(microtime());
+            }
+            $image = $_POST['wpu_polls_image'][$i];
+            if (!is_numeric($image)) {
+                $image = 0;
+            }
+
+            $answers[] = array(
+                'answer' => trim(esc_html($answer)),
+                'uniqid' => trim(esc_html($uniqid)),
+                'image' => $image
+            );
+        }
+        update_post_meta($post_id, 'wpu_polls__answers', $answers);
+
+        /* Save question */
+        if (isset($_POST['wpu_polls_question'])) {
+            update_post_meta($post_id, 'wpu_polls__question', esc_html($_POST['wpu_polls_question']));
+        }
+
     }
 
 }
