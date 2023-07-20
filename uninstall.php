559c4a839b23966ce859881ee9f3c406b8d63ef5
Kevin <kevin@rocher-dev.com>
2023-07-19 20:43:51 -0400

    v 0.13.1
    Add uninstall

---
+++ uninstall.php
@@ -0,0 +1,28 @@
+<?php
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    die;
+}
+
+/* Delete options */
+$options = array(
+    'wpu_polls_options',
+    'wpu_polls_wpu_polls_version'
+);
+foreach ($options as $opt) {
+    delete_option($opt);
+    delete_site_option($opt);
+}
+
+/* Delete tables */
+global $wpdb;
+$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}wpu_polls");
+
+/* Delete all polls */
+$allposts = get_posts(array(
+    'post_type' => 'polls',
+    'numberposts' => -1,
+    'fields' => 'ids'
+));
+foreach ($allposts as $p) {
+    wp_delete_post($p, true);
+}
